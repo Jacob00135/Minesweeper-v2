cebbd67f9852ec26c5921f18e0b9b01212a1785a
Jacob <2428207444@qq.com>
2023-05-16 19:46:05 +0800

    打开零块

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 (() => {
     'use strict';
-    
+    // TODO 找字体图标：标旗子、标问号、踩雷、标记错误
+    /**
+     * 正常数字范围为[0, 8]，雷是-1，边界是-2
+     */
     let rows = 9;
     let cols = 9;
     let mines = 10;
-    let gameAreaArray;
+    let isFirstOpen = true;  // 是否是第一次打开方块
+    let gameAreaArray;  // 棋局逻辑数组
+    let blockArray;  // 棋局HTML结点数组
+    let remanentSafeBlock;  // 剩余的未翻开的安全方块数，若此变量为0，表示赢得游戏
     toggleTheme();
-    generateGameAreaHTML();
-    
+    generateGameArea();
+
     function toggleTheme() {
         /**
          * 设置初始主题：若系统是浅色模式，或者没有深色模式，则设置白色主题
@@ -29,56 +35,15 @@
         });
     }
     
-    function isFirstOpen() {
+    function generateGameArea() {
         /**
-         * 判断是否是第一次翻开方块
-         */
-        return document.querySelector('#game-area > table > tr > td > button.open') === null;
-    }
-    
-    function openBlock(e) {
-        /**
-         * 翻开一个方块
-         */
-        const btn = e.target;
-        if (inArray('open', btn.classList) > 0) {
-            return undefined;
-        }
-        if (isFirstOpen()) {
-            generateGameAreaArray();
-            generateMines(btn.row, btn.col);
-            generateNumber();
-            setGameAreaNumber();
-        }
-        btn.classList.add('open');
-        if (btn.num === 0) {
-            // TODO 翻开周围的数字雷
-        } else if (btn.num === -1) {
-            btn.classList.add('mine');
-            alert('你输了！下次好运！');
-        } else {
-            btn.setAttribute('data-number', String(btn.num));
-            btn.innerHTML = String(btn.num);
-            if (isWin()) {
-                alert('你赢了！');
-            }
-        }
-    }
-    
-    function generateGameAreaArray() {
-        /**
-         * 生成游戏棋盘的逻辑数组
-         */
-        gameAreaArray = get2DArray(rows + 2, cols + 2, 0);
-    }
-    
-    function generateGameAreaHTML() {
-        /**
-         * 生成游戏棋盘的HTML
+         * 生成游戏棋局的HTML结点
          */
         const table = document.querySelector('#game-area > table');
+        blockArray = [];
         for (let r = 1; r < rows + 1; r++) {
             let tr = document.createElement('tr');
+            let blockRow = [];
             for (let c = 1; c < cols + 1; c++) {
                 let td = document.createElement('td');
                 let btn = document.createElement('button');
@@ -88,25 +53,108 @@
                 btn.col = c;
                 td.appendChild(btn);
                 tr.appendChild(td);
+                blockRow.push(btn);
             }
             table.appendChild(tr);
+            blockArray.push(blockRow);
         }
     }
-    
-    function gaToOne(r, c) {
+
+    function openMine(btn) {
         /**
-         * 棋局下标转换成一维下标：(r, c) --> (r - 1) * cols + c - 1
+         * 翻开雷块
          */
-        return (r - 1) * cols + c - 1;
+        btn.classList.add('mine');
+        alert('你输了！下次好运！');
     }
     
-    function oneToGa(i) {
+    function openNumber(btn) {
         /**
-         * 一维下标转换成棋局下标：i --> (parseInt(i / cols) + 1, i % cols + 1)
+         * 翻开数字块
          */
-        return [parseInt(i / cols) + 1, i % cols + 1];
+        btn.innerHTML = String(btn.num);
+        btn.setAttribute('data-number', String(btn.num));
+        remanentSafeBlock = remanentSafeBlock - 1;
     }
-    
+
+    function openZero(btn) {
+        /**
+         * 翻开数字零的块，形成翻开周围数字块的连锁反应
+         */
+        const queue = [btn];
+        remanentSafeBlock = remanentSafeBlock - 1;
+        while (queue.length > 0) {
+            let centerBlock = queue.shift();
+            for (let deltaR = -1; deltaR <= 1; deltaR++) {
+                for (let deltaC = -1; deltaC <= 1; deltaC++) {
+                    if (deltaR === 0 && deltaC === 0) {
+                        continue;
+                    }
+                    let r = centerBlock.row + deltaR;
+                    let c = centerBlock.col + deltaC;
+                    if (gameAreaArray[r][c] === -2 || blockArray[r - 1][c - 1].open) {
+                        continue;
+                    }
+                    let childBlock = blockArray[r - 1][c - 1];
+                    childBlock.classList.add('open');
+                    childBlock.open = true;
+                    if (childBlock.num === 0) {
+                        remanentSafeBlock = remanentSafeBlock - 1;
+                        queue.push(childBlock);
+                    } else {
+                        openNumber(childBlock);
+                    }
+                }
+            }
+        }
+    }
+
+    function openBlock(e) {
+        /**
+         * 翻开一个方块
+         */
+        const btn = e.target;
+        if (btn.open) {
+            return undefined;
+        }
+        if (isFirstOpen) {
+            isFirstOpen = false;
+            generateGameAreaArray();
+            generateMines(btn.row, btn.col);
+            generateNumber();
+            setGameAreaArrayToHTML();
+            remanentSafeBlock = rows * cols - mines;
+        }
+        btn.classList.add('open');
+        btn.open = true;
+        if (btn.num === -1) {
+            openMine(btn);
+            return undefined;
+        }
+        if (btn.num === 0) {
+            openZero(btn);
+        } else {
+            openNumber(btn);
+        }
+        if (remanentSafeBlock === 0) {
+            alert('你赢了！');
+        }
+    }
+
+    function generateGameAreaArray() {
+        /**
+         * 生成游戏棋盘的逻辑数组
+         */
+        gameAreaArray = get2DArray(rows + 2, cols + 2, 0);
+        for (let r = 0; r < rows + 2; r++) {
+            for (let c = 0; c < cols + 2; c++) {
+                if (r === 0 || r == rows + 1 || c === 0 || c === cols + 1) {
+                    gameAreaArray[r][c] = -2;
+                }
+            }
+        }
+    }
+
     function generateMines(firstRow, firstCol) {
         /**
          * 生成雷
@@ -121,7 +169,7 @@
             for (let deltaC = 1; deltaC >= -1; deltaC--) {
                 let r = firstRow + deltaR;
                 let c = firstCol + deltaC;
-                if (r >= 1 && r <= rows && c >= 1 && c <= cols) {
+                if (gameAreaArray[r][c] !== -2) {
                     deleteFromArray(indexList, gaToOne(r, c));
                 }
             }
@@ -141,7 +189,7 @@
             gameAreaArray[r][c] = -1;
         }
     }
-    
+
     function generateNumber() {
         /**
          * 根据逻辑数组生成数字块
@@ -161,25 +209,29 @@
             }
         }
     }
-    
-    function setGameAreaNumber() {
+
+    function setGameAreaArrayToHTML() {
         /**
          * 将逻辑数组的数值设置到页面的按钮中
          */
-        const trArray = document.querySelectorAll('#game-area > table > tr');
         for (let r = 0; r < rows; r++) {
-            let btnArray = trArray[r].querySelectorAll('td > button');
             for (let c = 0; c < cols; c++) {
-                let btn = btnArray[c];
-                btn.num = gameAreaArray[r + 1][c + 1];
+                blockArray[r][c].num = gameAreaArray[r + 1][c + 1];
             }
         }
     }
-    
-    function isWin() {
+
+    function gaToOne(r, c) {
         /**
-         * 判断游戏是否赢了
+         * 棋局下标转换成一维下标：(r, c) --> (r - 1) * cols + c - 1
          */
-        return document.querySelectorAll('#game-area > table > tr > td > button:not(.open)').length === mines;
+        return (r - 1) * cols + c - 1;
+    }
+    
+    function oneToGa(i) {
+        /**
+         * 一维下标转换成棋局下标：i --> (parseInt(i / cols) + 1, i % cols + 1)
+         */
+        return [parseInt(i / cols) + 1, i % cols + 1];
     }
 })();
